db736f7138946b5efd4efaf2ddf07fb848f5478c
linhongkuan <linhk8@mail2.sysu.edu.cn>
2025-12-25 05:30:53 +0800

    push

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  },
-  base: '/' 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    base: '/' 
+  };
 });
